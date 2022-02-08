91420ce3c809ccb7a19dbb9a9ab11584596b39dc
Hojii <HojatGazestani@gmail.com>
2022-02-08 16:24:10 +0330

    Create md file

--- Django/DjangoForBegin/blog-bck/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from .models import Post
-from django.views.generic import ListView
-
-class BlogListView(ListView):
-    model = Post
-    template_name = 'home.html'
